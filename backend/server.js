c2e6785e1357f6aa9d07405b0fa13b170c2cb85c
Nikhil Dhoke <nikhildhoke1995@gmail.com>
2026-05-11 17:12:48 +0100

    Set up Express backend

---
+++ backend/server.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const cors = require("cors");
+
+require("dotenv").config();
+
+const app = express();
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+
+// Test route
+app.get("/", (req, res) => {
+    res.send("PumpingIron API is running...")
+});
+
+// PORT
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+    console.log(`Server runnning on port ${PORT}`)
+});
