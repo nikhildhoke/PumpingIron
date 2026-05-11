80bdd9e6362314e35552abb8ef54cb9b54cfa60e
Nikhil Dhoke <nikhildhoke1995@gmail.com>
2026-05-11 22:07:02 +0100

    Connect backend to Mongo

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,12 @@
 const express = require("express");
 const cors = require("cors");
-
 require("dotenv").config();
+
+
+const connectDB = require("./config/db");
+
+// Connect to database  
+connectDB();
 
 const app = express();
 
